4ab7e03fda2548c6ab4d8b52647e9cc37a690d4a
Makha Blu Wakpa <bluwakpa@Makhas-MacBook-Pro.local>
2020-12-08 17:08:23 -0800

    fetch

--- app.js
+++ app.js
@@ -1,27 +1,41 @@
 'use strict';
 
-function getDogImage() {
-  fetch('https://dog.ceo/api/breeds/image/random')
+// const STORE = {
+//     number: 0,
+//     url: 
+// };
+
+function getDogImage(numberOfDogs) {
+  fetch(`https://dog.ceo/api/breeds/image/random/${numberOfDogs}`)
     .then(response => response.json())
     .then(responseJson => 
       displayResults(responseJson))
     .catch(error => alert('Something went wrong. Try again later.'));
+    console.log(numberOfDogs + " dogs");
 }
 
 function displayResults(responseJson) {
   console.log(responseJson);
-  //replace the existing image with the new one
-  $('.results-img').replaceWith(
-    `<img src="${responseJson.message}" class="results-img">`
-  )
-  //display the results section
-  $('.results').removeClass('hidden');
+  let html = '';
+  let num = $('input[type="number"]').val();
+  for (let i=0; i<responseJson.message.length; i++) {
+    html += `<img src="${responseJson.message[i]}" />`
+  }
+  $('.results-img').html(html)
+  $('.results h2').html(`Now showing you ${num} pictures of dogs.`);
+   //replace the existing image with the new one
+//    $('.results-img').replaceWith(
+//      `<img src="${html}" class="results-img">`
+//    )
+   //display the results section
+   $('.results').removeClass('hidden');
 }
 
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    getDogImage();
+    let numberOfDogs = $('input[type="number"]').val()
+    getDogImage(numberOfDogs);
   });
 }
 
